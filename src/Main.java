a695b015cbb428d315029c22bd00cae10defda37
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-12 17:15:38 +0300

    Ex1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] expenses = {120017, 120041, 120049, 120077, 120103};
+        int sumExpenses = 0;
+        for (int index = 0; index < expenses.length; index++) {
+            sumExpenses += expenses[index];
+        }
+        System.out.println("Сумма затрат компании за месяц составит " + sumExpenses + " рублей.");
     }
 }
