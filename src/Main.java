204f4ffa4eeeca8685d24ecfb44b94ef37825273
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-12 17:43:31 +0300

    Ex2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Example1");
         int[] expenses = {120017, 120041, 120049, 120077, 120103};
         int sumExpenses = 0;
         for (int index = 0; index < expenses.length; index++) {
             sumExpenses += expenses[index];
         }
         System.out.println("Сумма затрат компании за месяц составит " + sumExpenses + " рублей.");
+        System.out.println("Example2");
+        int[] expensesSecond = {113002, 107007, 101011, 103007, 100903};
+        int maxExpense = 0;
+        for (final int current : expensesSecond) {
+            if (current > maxExpense) {
+                maxExpense = current;
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю составляет " + maxExpense + " рублей.");
+        int minExpense = 0;
+        for (int index = 0; index < expensesSecond.length; index++) {
+            minExpense = expensesSecond[index];
+            if (minExpense > expensesSecond[index + 1]) {
+                minExpense = expensesSecond[index];
+            } else
+                break;
+        }
+        System.out.println("Минимальная сумма трат за неделю составляет " + minExpense + " рублей.");
     }
 }
